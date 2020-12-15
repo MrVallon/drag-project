6b11d69633f1744070c277efb540815a52d3995e
MrVallon <vallonkings@gmail.com>
2020-12-16 01:59:56 +0200

    new changes in configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,76 +10,76 @@ const isDev = !isProd;
 const fileName = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
 
 const jsLoaders = () => {
-  const loaders = [
-    {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-typescript', '@babel/preset-react'],
-      },
-    },
-  ];
-  if (isDev) {
-    loaders.push('eslint-loader');
-  }
-  return loaders;
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-typescript', '@babel/preset-react'],
+            },
+        },
+    ];
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+    return loaders;
 };
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: ['@babel/polyfill', './index.tsx'],
-  output: {
-    filename: fileName('js'),
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@component': path.resolve(__dirname, 'src/components'),
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './index.tsx'],
+    output: {
+        filename: fileName('js'),
+        path: path.resolve(__dirname, 'dist'),
     },
-  },
-  target: process.env.NODE_ENV === 'development' ? 'web' : 'browserslist',
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 3000,
-    hot: isDev,
-    overlay: true,
-    open: true,
-    inline: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      template: 'index.html',
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@component': path.resolve(__dirname, 'src/components'),
         },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: fileName('css'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(js|jsx|tsx|ts)$/,
-        exclude: /node_modules/,
-        use: jsLoaders(),
-      },
+    },
+    target: process.env.NODE_ENV === 'development' ? 'web' : 'browserslist',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3000,
+        hot: isDev,
+        overlay: true,
+        open: true,
+        inline: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            },
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: fileName('css'),
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                use: jsLoaders(),
+            },
+        ],
+    },
 };
