49ea7594adb6eba19e5ff600f7d696745b8ca3c7
Sergei <53310278+MrVallon@users.noreply.github.com>
2020-12-21 01:13:20 +0200

    Refactoring class

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import './Loader.scss';
 const Loader = () => (
     <div className="loading">
         <svg>
-            <circle className="dot__circ" cx="40" cy="40" r="39.5" />
+            <circle cx="40" cy="40" r="39.5" />
         </svg>
     </div>
 );
